cb1e97503069c9f87f694af615623318bfc29d44
SonicGD <sonicgd@gmail.com>
2014-07-04 22:21:01 +0600

    use yii-advanced

--- common/config/main.php
+++ common/config/main.php
@@ -1,24 +1,9 @@
 <?php
 return [
-    'id' => 'BioEngine console',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'console\controllers',
-    'modules' => [],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
     ],
-    'params' => [],
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
 ];
