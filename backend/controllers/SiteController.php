599a142ac982fda9a503c27bacf16cafb1f4d47e
SonicGD <sonicgd@gmail.com>
2014-07-07 14:15:50 +0600

    add backend

---
+++ backend/controllers/SiteController.php
@@ -0,0 +1,17 @@
+<?php
+namespace bioengine\backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
---
+++ backend/views/site/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Георгий
+ * Date: 07.07.2014
+ * Time: 14:15
+ */ 
