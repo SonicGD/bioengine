99e0b16526e7908e494dd999911cddd6a9cbc461
SonicGD <sonicgd@gmail.com>
2015-02-01 17:01:24 +0500

    deny guest access

--- common/components/BackendController.php
+++ common/components/BackendController.php
@@ -3,7 +3,29 @@
 namespace bioengine\common\components;
 
 
+use bioengine\common\helpers\UserHelper;
+use bioengine\common\modules\ipb\models\IpbMember;
+use yii\helpers\Url;
+
+/**
+ * Class BackendController
+ * @package bioengine\common\components
+ *
+ */
 class BackendController extends Controller
 {
 
+    /**
+     * @var IpbMember
+     */
+    public $user;
+
+    public function init()
+    {
+        if (\Yii::$app->user->isGuest) {
+            $this->redirect(Url::toRoute('/login/index'));
+        }
+
+        $this->user = UserHelper::getUser();
+    }
 } 
