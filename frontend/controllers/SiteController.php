70bb5edac14e1e4a1bd9efdadecdb231db57f0bd
SonicGD <sonicgd@gmail.com>
2015-06-13 13:40:14 +0500

    add login method

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -2,6 +2,7 @@
 namespace bioengine\frontend\controllers;
 
 use bioengine\frontend\components\Controller;
+use yii\web\Response;
 
 /**
  * Site controller
@@ -12,5 +13,11 @@ class SiteController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public function actionLogin($login, $password)
+    {
+        \Yii::$app->response->format = Response::FORMAT_JSON;
+        return ['result' => false];
     }
 }
