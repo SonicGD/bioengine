584ce4ae5418d630f601aecd70bab39c4ae7df35
SonicGD <sonicgd@gmail.com>
2015-07-15 08:55:58 +0500

    support old urls

--- common/modules/articles/config/rules.php
+++ common/modules/articles/config/rules.php
@@ -3,6 +3,7 @@
 return [
     '/<parentUrl:\w+>/articles'                                              => '/articles/root',
     '/<parentUrl:\w+>/articles/<catUrl:[a-zA-Z0-9_\/]+>/<articleUrl:[a-zA-Z0-9_-]+>' => '/articles/show',
+    '/articles/<parentUrl:\w+>/<catUrl:[a-zA-Z0-9_\/]+>/<articleUrl:[a-zA-Z0-9_-]+>' => '/articles/show',
     [
         'pattern'      => '/<parentUrl:\w+>/articles/<catUrl:[a-zA-Z0-9_\/]+>',
         'route'        => '/articles/cat',
