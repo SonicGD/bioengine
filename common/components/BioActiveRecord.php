57ea7ef02031fb8dc76d82b87cdc6ce9fb48c8ad
SonicGD <sonicgd@gmail.com>
2015-07-18 14:07:56 +0500

    use ar-cache

--- common/components/BioActiveRecord.php
+++ common/components/BioActiveRecord.php
@@ -3,10 +3,13 @@
 namespace bioengine\common\components;
 
 
+use sitkoru\cache\ar\ActiveRecordTrait;
 use yii\db\ActiveRecord;
 
 class BioActiveRecord extends ActiveRecord
 {
+    use ActiveRecordTrait;
+
     public function getPublicUrl($absolute = false)
     {
         return $absolute;
