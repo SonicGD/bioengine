707d6792731ea2c452edc155631d2b796c934662
SonicGD <sonicgd@gmail.com>
2015-07-26 15:21:35 +0500

    add date rules for news

--- common/modules/news/config/rules.php
+++ common/modules/news/config/rules.php
@@ -11,8 +11,14 @@ return [
         'route'        => '/site/root',
         'encodeParams' => false
     ],
-    '/<year:\d+>/<month:\d+>/<day:\d+>/<newsUrl:(.)+>' => '/news/show',
-    '/page/<page:\d+>'                                => '/site/index',
-    '/rss'                                            => 'news/rss',
-    'news/update-forum-post'                          => 'news/update-forum-post'
+    '/<year:\d+>/<month:\d+>/<day:\d+>/<newsUrl:(.)+>'  => '/index/show',
+    '/<year:\d+>/<month:\d+>/<day:\d+>/page/<page:\d+>' => '/site/date',
+    '/<year:\d+>/<month:\d+>/<day:\d+>'                 => '/site/date',
+    '/<year:\d+>/<month:\d+>/page/<page:\d+>'           => '/site/date',
+    '/<year:\d+>/<month:\d+>'                           => '/site/date',
+    '/<year:\d+>/page/<page:\d+>'                       => '/site/date',
+    '/<year:\d+>'                                       => '/site/date',
+    '/page/<page:\d+>'                                  => '/site/index',
+    '/rss'                                              => 'news/rss',
+    'news/update-forum-post'                            => 'news/update-forum-post'
 ];
