eeb2ef37f55d7633d051450d74c5fb852daae119
SonicGD <sonicgd@gmail.com>
2016-07-03 02:49:44 +0500

    upd to ipb4 urls

--- common/modules/news/models/News.php
+++ common/modules/news/models/News.php
@@ -272,7 +272,7 @@ class News extends BioActiveRecord
 
     public function getForumUrl()
     {
-        return '/forum/topic/' . $this->tid . '/page__view__getnewpost';
+        return \Yii::$app->params['forum_url'] . 'topic/' . $this->tid . '/?do=getNewComment';
     }
 
     public function getCommentsText()
