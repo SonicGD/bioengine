4fb2c7f2687de6a1f54626110a8a601ee3a51533
SonicGD <sonicgd@gmail.com>
2016-07-03 05:01:32 +0500

    add settings to console controllers

--- console/components/Controller.php
+++ console/components/Controller.php
@@ -5,5 +5,19 @@ namespace bioengine\console\components;
 
 class Controller extends \yii\console\Controller
 {
+    public $settings = [];
 
+    public function Init()
+    {
+        parent::init();
+
+        /**
+         * @var Settings[] $settings
+         */
+        if ($settings = Settings::find()->all()) {
+            foreach ($settings as $setting) {
+                $this->settings[$setting->name] = $setting->value;
+            }
+        }
+    }
 } 
